e37b4b4c664357e667835680558e89a97fc688b2
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-08-20 21:31:12 -0700

    more basic styling

--- photo-app/src/app/app.routes.ts
+++ photo-app/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home',
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent,
+    title: 'Gallery',
   },
 ];
