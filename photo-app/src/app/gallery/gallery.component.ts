3423865186d00c7daf18524e978b648d15266bc1
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-08-23 21:49:14 -0700

    add in PhotoService and cards for displaying photos

--- photo-app/src/app/gallery/gallery.component.ts
+++ photo-app/src/app/gallery/gallery.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+import { PhotoService } from '../services/photoService';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.scss'
+  styleUrl: './gallery.component.scss',
 })
 export class GalleryComponent {
+  constructor(private photoService: PhotoService) {}
 
+  name: string = 'GalleryComponent';
+
+  photos: any[] = [];
+
+  ngOnInit() {
+    this.photoService.getPhotos().subscribe((photos) => {
+      this.photos = photos;
+    });
+  }
 }
